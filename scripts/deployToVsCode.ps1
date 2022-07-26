c9fd7ca7264ffe21d6f5c229a32881fa098df28a
dannoe <github@dannoe.de>
2022-07-26 11:30:25 +0200

    Fixed typo in path

--- scripts/deployToVsCode.ps1
+++ scripts/deployToVsCode.ps1
@@ -4,5 +4,5 @@ param
 )
 
 Stop-Process -Name "Microsoft.Dynamics.Nav.EditorServices.Host" -Force -ErrorAction Ignore
-$vscodeALAnalyzerFolder = Get-Item "$env:USERPROFILE\.vscode\extensions\ms-dynamics-smb.al-*\bin\Analyzer" | Select -Last 1
+$vscodeALAnalyzerFolder = Get-Item "$env:USERPROFILE\.vscode\extensions\ms-dynamics-smb.al-*\bin\Analyzers" | Select -Last 1
 xcopy.exe /y /d $TargetPath $vscodeALAnalyzerFolder
